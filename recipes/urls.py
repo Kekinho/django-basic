2468ac0452dfc7bc8a6703914eaf53e61d4aa7f2
Eric Bernardes <bernardes.eric@gmail.com>
2024-05-24 22:36:01 -0300

    Terceiro Commmit, css, partials, html

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import home
+from recipes.views import home, sobre
 
 urlpatterns = [
     path('', home),
+    path('sobre/', sobre),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Eric Bernardes',
+        })
+
+def sobre(request):
+    return render(request, 'recipes/pages/sobre.html')
